873be29c8ec229d31005ddb1b0c505dedd06183f
slipper <slipper@0ops.net>
2018-08-09 10:32:50 -0700

    add test2: detect job difficulty update

---
+++ interaction/test2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+from pwn import *
+import sys
+
+from stratum import Stratum, CheckFailure, FLAG_PRICE_PER_BIT, FLAG_BITS, REAL_FLAG
+
+TIMEOUT = 60
+
+def main():
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    conn = remote(host, port)
+
+    s = Stratum(conn)
+    s.login()
+
+    # make sure we have a new target
+    for _ in xrange(10):
+        if s.target == 0:
+            time.sleep(0.1)
+    assert s.target is not None, 'no target received?'
+
+    # do nonthing and we expect new target every 15s
+
+    cur_target = s.target
+    end = time.time() + TIMEOUT
+    while time.time() < end:
+        time.sleep(16)
+        if s.target == cur_target:
+            raise CheckFailure('target not updated')
+        cur_target = s.target
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
+    
+
