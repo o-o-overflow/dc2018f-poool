ec70ea2eff4dfe8531267747e8c9bd24873eaa04
slipper <slipper@0ops.net>
2018-08-10 03:06:56 -0700

    update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 set -x
@@ -6,25 +6,22 @@ set -x
 # quick a dirty extract of the name from the info.yml
 SERVICE_NAME=$(cat info.yml | grep "^service_name:" | cut -f 2 -d '"')
 
-SERVICE_IMAGE="$SERVICE_NAME-service"
-INTERACTION_IMAGE="$SERVICE_NAME-interaction"
-SERVICE_TAG=$(git rev-parse HEAD)
+SERVICE_TAG="$SERVICE_NAME-service"
+INTERACTION_TAG="$SERVICE_NAME-interaction"
 
-cd service && docker build . -t "$SERVICE_IMAGE:$SERVICE_TAG"  && cd -
+cd service && docker build . -t "$SERVICE_TAG"  && cd -
 
-cd interaction && docker build . -t "$INTERACTION_IMAGE:$SERVICE_TAG" && cd -
+cd interaction && docker build . -t "$INTERACTION_TAG" && cd -
 
-SERVICE_ID=$(docker run -d --rm "$SERVICE_IMAGE:$SERVICE_TAG")
+SERVICE_ID=$(docker run -d --rm "$SERVICE_TAG")
 
 # TODO: EXPLOIT_SCRIPTS=$(get_info.py exploit_scripts)
-# EXPLOIT_SCRIPTS="/exploit1.py /exploit2.py /exploit3.py"
-# do not run exploit2.py which may requires external resource
 EXPLOIT_SCRIPTS="/exploit1.py /exploit3.py"
 
 
 # TODO: SLA_SCRIPTS=$(get_info.py sla_scripts)
-#SLA_SCRIPTS="/check1.py /check2.sh"
 SLA_SCRIPTS="/sla1.py"
+
 
 # TODO: SERVICE_PORT=$(get_info.py service_port)
 SERVICE_PORT=10001
@@ -33,17 +30,16 @@ SERVICE_PORT=10001
 
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$SERVICE_ID")
 
-for script in $EXPLOIT_SCRIPTS
-do
-	RESULT=$(docker run --rm "$INTERACTION_IMAGE:$SERVICE_TAG" "$script" "$IP" "$SERVICE_PORT")
-	echo "$RESULT" | grep "FLAG:"
-done
 
 for script in $SLA_SCRIPTS
 do
-	RESULT=$(docker run -it --rm "$INTERACTION_IMAGE:$SERVICE_TAG" "$script" "$IP" "$SERVICE_PORT")
+    docker run --rm "$INTERACTION_TAG" "$script" "$IP" "$SERVICE_PORT"
+done
+
+for script in $EXPLOIT_SCRIPTS
+do
+    RESULT=$(docker run --rm "$INTERACTION_TAG" "$script" "$IP" "$SERVICE_PORT")
+    echo $RESULT | grep "FLAG:"
 done
 
 docker kill "$SERVICE_ID"
-
-
